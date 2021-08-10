7c58dd5d45fe4257ceed2ed3af318db4d1ce04a1
Vipin Kadyan <84658301+VK-V-K@users.noreply.github.com>
2021-08-10 12:27:14 +0530

    Add files via upload

---
+++ precache-manifest.b84d92717028af5405ee0dd250173599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6550d41a67c53fbee7d",
+    "url": "https://vk-v-k.github.io/SearchEngine/static/css/main.44bf0282.chunk.css"
+  },
+  {
+    "revision": "e6550d41a67c53fbee7d",
+    "url": "https://vk-v-k.github.io/SearchEngine/static/js/main.18ed4ac1.chunk.js"
+  },
+  {
+    "revision": "dd12f5eff44ef385a3ca",
+    "url": "https://vk-v-k.github.io/SearchEngine/static/js/runtime~main.074b4b15.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "https://vk-v-k.github.io/SearchEngine/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "138a1e00a8a02094effa52e100eb2774",
+    "url": "https://vk-v-k.github.io/SearchEngine/static/media/img.138a1e00.jpg"
+  },
+  {
+    "revision": "35d48b2249aba5ba5d2aecab4e008a94",
+    "url": "https://vk-v-k.github.io/SearchEngine/index.html"
+  }
+];
